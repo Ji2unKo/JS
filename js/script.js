369b0e7be667c2c63c28bbb3032347dd4dee587d
Ji2unKo <wldnjs4983@naver.com>
2023-08-29 00:07:01 +0900

    ✨ alert 버튼 , 닫기

---
+++ js/script.js
@@ -0,0 +1,12 @@
+    // 웹 문서 -> document .-> 뭐뭐안에~ / ~의
+    // getElementById id hello인 html요소 가져와 innerHTML->내부글자
+    document.getElementById('hello').innerHTML ='안녕';
+    // 셀렉터 selector
+    // 컴퓨터야 id = hi 라는 애들 올때 바밤바로 변경해줘
+    document.getElementById('hi').innerHTML = '올때 바밤바';
+    document.getElementById('hello').style.color = 'red';
+    document.getElementById('hi').style.fontSize ='30px';
+
+
+    // Alert
+    document.getElementById('alert').style.display = 'none';
--- script.js
+++
@@ -1,4 +0,0 @@
-    // 웹 문서 -> document .-> 뭐뭐안에~ / ~의
-    // getElementById id hello인 html요소 가져와 innerHTML->내부글자
-    document.getElementById('hello').innerHTML ='안녕';
-    document.getElementById('hi').innerHTML = '올때 바밤바';
