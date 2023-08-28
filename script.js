664d563bdad085dd08f50f8685988ef556d59f64
Ji2unKo <wldnjs4983@naver.com>
2023-08-28 23:50:24 +0900

    ✨ js 폴더 생성

---
+++ script.js
@@ -0,0 +1,4 @@
+    // 웹 문서 -> document .-> 뭐뭐안에~ / ~의
+    // getElementById id hello인 html요소 가져와 innerHTML->내부글자
+    document.getElementById('hello').innerHTML ='안녕';
+    document.getElementById('hi').innerHTML = '올때 바밤바';
