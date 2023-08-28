7e8334ae3694e7689c3d36082e03bec980000cb6
Ji2unKo <wldnjs4983@naver.com>
2023-08-29 01:03:07 +0900

    ✨ jQuery 으로 구현

---
+++ js/jQuery.js
@@ -0,0 +1,9 @@
+
+// document.getElementById('test).innerHTML = '???';
+// document.querySelector('#test') -> 한개만 찾는다 all -> 여러개
+// jQuery  id=># $('css셀렉터'), class -> .을 붙힌다 $('.test1)
+$('#test').html('안녕');
+$('#test').css('color', 'red');
+// attr -> 이걸, 이렇게
+$('#test').attr('src', 'dsddd');
+
