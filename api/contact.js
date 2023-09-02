ad26014223ffb8c56b184d8be6605625891ed57f
Ji2unKo <wldnjs4983@naver.com>
2023-09-02 21:34:22 +0900

    ✨ othersBtn

---
+++ api/contact.js
@@ -0,0 +1,58 @@
+let profile = [
+  {
+    img : 'https://avatars.githubusercontent.com/u/132882346?v=4',
+    name : 'SEOWOO CHOI',
+    txt : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? Repellendus deleniti saepe eum ipsum explicabo porro. Laborum rerum maxime odio, blanditiis molestias aliquam labore.',
+    github : 'https://github.com/choiseowu',
+    email : 'chltjdn95@nate.com',
+  },
+  {
+    img : 'https://avatars.githubusercontent.com/u/132882429?v=4',
+    name : 'JIEUN KO',
+    txt : '안녕하세요! 저는 디디쥬에서 section3, today, contact, 코스대전, api를 맡아서 작업을 했습니다. <br><br>  초반 기획부터 팀원들과 맡은 부분에 프로젝트를 했습니다. 프로젝트 특성상 기간도 짧지만 노션에 팀과 나눠서 할 코드도 올려보고 특히 깃허브에서 머지를 할 때 부분에 대해 충돌 한번 나고서는 그 후로 서로 게속 카톡로 수시로 대화도 했습니다. 일찍 나와서 회의도 하고 코드도 짜면서 기능 구현을 했습니다. <br><br> 프로젝트를 하면서 어려웠던 부분이라면 수업시간에 배운걸로 코드를 짜서 완성을 하고 회의를 할 때 보면 기능을 더 추가하는 방법은 어떤지도 의견을 받고 디자인 변동도 많아서 수업시간외에 구글링으로 배운공부들도 있었기 때문에 오히려 공부하는데 더 도움이 되었습니다. 팀프로젝트를 하면서 가장 많이 성장할수 있는 부분인거 같습니다.<br><br>  다같이 부족한 부분 오류도 생겨서 잡고 같이 구현하면서 시간도 보냈습니다. 팀원들과 원할게 소통도 잘되어서 너무 좋았습니다!! ',
+    github : 'https://github.com/Ji2unKo',
+    email : 'wldnjs4983@naver.com',
+  },
+  {
+    img : 'https://avatars.githubusercontent.com/u/132882278?v=4',
+    name : 'HANNA OH',
+    txt : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? RepellendusUt pariatur fugit saepe possimus accusamus facere? Repellendus deleniti saepe eum ipsum explicabo porro. Laborum rerum maxime odio, blanditiis molestias aliquam labore.',
+    github : 'https://github.com/hanna0115',
+    email : 'hanna0115@naver.com',
+  },
+  {
+    img : 'https://avatars.githubusercontent.com/u/132882475?v=4',
+    name : 'KAI',
+    txt : '안녕하세요. 저는 DDju 팀 협업 프로젝트에서 회원가입, 로그인, ID/PW 찾기, 코스 상세 설명 페이지 및 index.html의 footer를 맡은 Kai입니다. <br><br> 프로젝트 특성상 짧은 기간이 주어져 걱정이 많았으나, 워낙 좋은 팀을 만나 팀원들과 상의하며 하나하나 만들어 나가는 기쁨과 설렘이 컸어요. 그렇게 시작된 첫 번째 프로젝트, "대전의 온도". <br><br> 특별한 계획 없이 좋은 것들만 모아 구성하려던 우리의 계획 아닌 계획은 중구난방 구현된 기능을 하나로 합치려던 순간, 무수히 뜨는 에러 메시지와 함께 공중분해 될 뻔했는데요. 당시 GitHub branch 시스템에 대해 배우기 전이라 수동으로 작업했음에도 오류가 대량 발생하여, 이때 대대적인 개혁이 필요하다는 것을 깨닫고 팀원 모두 한데 모여 두 번째 프로젝트를 계획하게 되었습니다. <br><br> 한나님을 필두로 피그마를 통해 기획이 구성으로, 지은님과 서우님에 의해 구성에서 설계로 나아가며 전보다 나은 구조적 형태를 갖추면서 프로젝트가 순조롭게 순항하는 듯했습니다. <br><br> API라는 벽을 만나기 전까지는요. 프로젝트에서 가장 중요한 구심점인 API라는 벽을 다 함께 똘똘 뭉쳐 깨부수면서 새롭게 등장한 Node.js와 MongoDB를 넘고 넘어 지금에 이르렀네요. <br><br> 필요할 때마다 카톡과 같은 SNS로 대화하고 Nohion을 통한 자료 공유, 일정 시간 모여 회의하면서 기반을 닦았고 각자 맡은 바 역할에 집중하여 만들어낸 오늘이요. <br><br> 자, 아직도 우리는 해야 할 일이 많습니다. 더 만들어야 할 페이지도 남아있고, 기획했던 기능도 구현해야 할 부분이 많습니다. 상업 프로젝트였다면, 최소한 세 번 업데이트할 만큼 기획한 분량이 많지만, 부족한 시간에도 불구하고 팀원 모두 똘똘 뭉친 결과, 원하는 목표에 근접할 수 있었다고 생각합니다. <br><br> 좋은 결과를 낼 수 있게 도와준 모두에게 감사를 돌리며 마지막까지 스크롤 내려준 여러분에게도 고맙습니다. 더 나은 업데이트를 위해, 파이팅!',
+    github : 'https://github.com/biabamroi',
+    email : 'biabamroi@gmail.com',
+  },
+]
+
+
+let othersBtn = document.querySelectorAll('.others-btn button');
+
+othersBtn.forEach(function(btn, index){
+  btn.addEventListener('click', function(){
+    othersBtn.forEach(function(item){
+      item.classList.remove('on');
+    })
+    btn.classList.add('on');
+    document.querySelector('.profile img').src = profile[index].img;
+    document.querySelector('.profile img').alt = profile[index].title;
+    document.querySelector('.profile h1').innerHTML = profile[index].name;
+    document.querySelector('.txt p').innerHTML = profile[index].txt;
+
+    let liTag = document.querySelectorAll('.contact li');
+    liTag[0].querySelector('a').href = profile[index].github;
+    liTag[1].querySelector('span').innerHTML = profile[index].email;
+  })
+})
+
+
+document.querySelector('.profiles-btn').addEventListener('click', function(){
+  document.querySelector('video').style.opacity = '.5'
+  document.querySelector('video').muted = true;
+  document.querySelector('.profile-wrapper').style.display = 'flex';
+})
+
