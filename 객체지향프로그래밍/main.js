3528983e847cbc1cb7033b62bae2caf49adf3dfb
Ji2unKo <wldnjs4983@naver.com>
2023-09-23 18:55:43 +0900

    ✨ es6 객체지향

--- 객체지향프로그래밍/main.js
+++ 객체지향프로그래밍/main.js
@@ -1,10 +1,10 @@
-const box1 = document.querySelector('.box1');
+/*const box1 = document.querySelector('.box1');
 const box2 = document.querySelector('.box2');
 const box3 = document.querySelector('.box3');
 
-/*box1.addEventListener('click', e => {
+box1.addEventListener('click', e => {
   box1.style.backgroundColor = 'hotpink';
-})*/
+})
 
 box1.addEventListener('click', e => changBg(box1))
 
@@ -12,6 +12,30 @@ box2.addEventListener('click', e => changBg(box2))
 
 box3.addEventListener('click', e => changBg(box3))
 
+
 function changBg(selector){
   selector.style.backgroundColor = 'hotponk';
 }
+*/
+
+
+class Box {
+  constructor(selector){
+    this.el = document.querySelector(selector); 
+    this.el.addEventListener('click', ()=>this.changeBg(this.el) );
+  }
+  changeBg(selector){
+    selector.style.backgroundColor = 'hotpink';
+}
+}
+
+new Box('.box1');
+new Box('.box2');
+new Box('.box3');
+
+/*
+생성자 함수 : 틀을 만드는 것
+인스턴스 (객체) (instance) : 틀을 통해서 복사물
+프로토타입 (prototype) : 공용 저장 창고 <생성장 함수를 통해 복사물>
+*/
+
