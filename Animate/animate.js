3021beee9aacc84793d5a2976a38affdc9d51b5c
Ji2unKo <wldnjs4983@naver.com>
2023-09-23 21:04:04 +0900

    ✨ 반복횟수별 누적시간 계산

--- Animate/animate.js
+++ Animate/animate.js
@@ -10,17 +10,22 @@ requestAnimationFrame - 화면주사율에 맞게 자동 반복기능
 const btn =  document.querySelector('button');
 const box =  document.querySelector('#box');
 let num = 0;
+let startTime = null;
 
 btn.addEventListener('click', e=>{
   requestAnimationFrame(move);
+  startTime = performance.now();
 })
 
-function move(){
-  if(num < 200){
+function move(time){
+  let timeLast = time - startTime;
+  if(num < 100){
     num++;
     requestAnimationFrame(move)
+    console.log(`반복횟수 : ${num}`);
+    console.log(`회차별 누적시간 : ${timeLast}`);
   } else {
-    num = 200;
+    num = 100;
   }
-  box.style.marginLeft = num+'px';
+  //box.style.marginLeft = num+'px';
 }
